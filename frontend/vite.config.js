c9ec04c4f936b86c2790369ad3af6b088147b8f4
tadeowoj <tadeo@zsoiz-czyzew.pl>
2026-04-11 14:06:13 +0200

    feat: configure Vite base path and dev server settings for network access

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/dni-otwarte-QRCode/', // Umożliwia hostowanie na ścieżkach wymuszanych przez Github Pages
+  base: '/', // Główna domena qr.zsoiz-czyzew.pl
 
   server: {
     host: true, // Słuchaj na wszystkich adresach lokalnych (umożliwia test ze smartfona pzez wi-fi)
